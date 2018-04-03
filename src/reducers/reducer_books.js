6fe876cba2646bf4e565a282fc1882aa1b68ac36
Muhammad Azri <muhd.azri@fiction-labs.com>
2018-04-03 15:49:37 +0800

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+      { title: 'Javascript: The Good Part And The Bad Part' },
+      { title: 'Harry Potter' },
+      { title: 'Toy Story' },
+      { title: '1Q84' }
+    ]
+  }
